e1396fc8e5d92209733ae6ac149461853b1d0f49
dotX12 <hash@shitposting.team>
2021-03-09 13:53:21 +0300

    API urls

---
+++ urls.py
@@ -0,0 +1,4 @@
+MARKET_URL = ('https://api.coingecko.com/api/v3/coins/'
+              'markets?vs_currency={}&ids={}&order={}&'
+              'per_page=100&page=1&sparkline={}&'
+              'price_change_percentage={}')
